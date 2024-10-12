c0c350172f2922dc70ced499f1245a85330c5e81
pravallikanaga305 <pravallikanaga3@gmail.com>
2024-10-12 18:53:33 -0500

    demo on merge conflicts

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,6 +18,7 @@ public class Main {
            System.out.println("merge2 example commit");
         System.out.println("reset2 example commit-1");
 
+        System.out.println("merge conflict-1");
 
 
 
